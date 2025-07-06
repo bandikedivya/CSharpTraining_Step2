76de0923dad5f9d310773c120025c8e68c65a5de
divyabandikee <divyabandike.fullstack@gmail.com>
2025-07-06 22:08:34 +0530

    Code last changes made

---
+++ Step2/7.StaticVsNonStatic/1.Instantiate.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+namespace StaticAndNonStaticInstatiate
+{
+    public static class StaticClass
+    {
+        public static void Static()
+        {
+            // string text = "This is a static class method";
+            Console.WriteLine("This is a static class method");
+        }
+    }
+    public class NonStaticClass
+    {
+
+        public void NonStaticClassWithNonStaticMethod()
+        {
+            string text1 = "This is a non static class, non static method which requires instance to call in the main method";
+            Console.WriteLine(text1);
+        }
+
+        public static void NonStaticClassWithStaticMethod()
+        {
+            string text2 = "This is a non static class, static method which requires instance to call in the main method";
+            Console.WriteLine(text2);
+        }
+    }
+   
+    public class MainProgram
+    {
+        public static void Main()
+        {
+            StaticClass.Static();
+
+
+            NonStaticClass nonStaticClass = new NonStaticClass();
+            nonStaticClass.NonStaticClassWithNonStaticMethod();
+
+            NonStaticClass.NonStaticClassWithStaticMethod();
+
+
+
+            
+
+
+        }
+
+
+    }
+}
---
+++ Step2/7.StaticVsNonStatic/2.Member.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace StaticAndNonStaticInstatiate
+{
+    public static class StaticRestaurantLogger
+    {
+       public static int ChefList = 0;
+
+
+        /// <summary>
+        ///  Log message is for print the Message as well as increase +1 the value
+        ///  Example : Incase EmployeesList is 2 then it will return 3
+        /// </summary>
+        /// <param name="message"></param>
+        /// 
+        public static void Joinedrestaurant(string message)
+        {
+            Console.WriteLine($"{message} Joined Restaurant on {DateTime.Now}");
+            ChefList = ChefList + 1;
+        }
+        public static int GetChefListCount()
+        {
+            return ChefList; // returns the latest chef list count
+        }
+
+    }
+    public  class NonStaticRestaurantLogger
+    {
+        public static int ChefList = 0;
+
+
+        /// <summary>
+        ///  Log message is for print the Message as well as increase +1 the value
+        ///  Example : Incase EmployeesList is 2 then it will return 3
+        /// </summary>
+        /// <param name="message"></param>
+        /// 
+        public void Joinedrestaurant(string message)
+        {
+            Console.WriteLine($"{message} Joined Restaurant on {DateTime.Now}");
+            ChefList = ChefList + 1;
+        }
+        public  int GetChefListCount()
+        {
+            return ChefList; // returns the latest chef list count
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            StaticRestaurantLogger.Joinedrestaurant("Master1 ");
+            StaticRestaurantLogger.Joinedrestaurant("Master2 ");
+            StaticRestaurantLogger.Joinedrestaurant("Master3 ");
+            StaticRestaurantLogger.GetChefListCount();
+            Console.WriteLine($"Total number of Chefs joined in my restaurant: {StaticRestaurantLogger.GetChefListCount()} ");
+
+
+            NonStaticRestaurantLogger nonStaticRestaurantLogger = new NonStaticRestaurantLogger();
+            nonStaticRestaurantLogger.Joinedrestaurant("Master1 ");
+            nonStaticRestaurantLogger.Joinedrestaurant("Master2 ");
+            nonStaticRestaurantLogger.Joinedrestaurant("Master3 ");
+            Console.WriteLine($"Total number of Chefs joined in my restaurant:{nonStaticRestaurantLogger.GetChefListCount()}");
+
+
+
+
+
+
+        }
+    }
+}
