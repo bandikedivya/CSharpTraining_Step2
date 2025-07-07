ca83de34f91e100b2deaf4ec1f03a53ec6327882
divyabandikee <divyabandike.fullstack@gmail.com>
2025-07-07 08:54:33 +0530

    changes made

--- Step2/7.StaticVsNonStatic/2.Member.cs
+++ Step2/7.StaticVsNonStatic/2.Member.cs
@@ -58,9 +58,17 @@ namespace StaticAndNonStaticInstatiate
 
             NonStaticRestaurantLogger nonStaticRestaurantLogger = new NonStaticRestaurantLogger();
             nonStaticRestaurantLogger.Joinedrestaurant("Master1 ");
-            nonStaticRestaurantLogger.Joinedrestaurant("Master2 ");
-            nonStaticRestaurantLogger.Joinedrestaurant("Master3 ");
             Console.WriteLine($"Total number of Chefs joined in my restaurant:{nonStaticRestaurantLogger.GetChefListCount()}");
+
+
+            NonStaticRestaurantLogger nonStaticRestaurantLogger1 = new NonStaticRestaurantLogger();
+            nonStaticRestaurantLogger1.Joinedrestaurant("Master2 ");
+            Console.WriteLine($"Total number of Chefs joined in my restaurant:{nonStaticRestaurantLogger1.GetChefListCount()}");
+
+
+            NonStaticRestaurantLogger nonStaticRestaurantLogger2 = new NonStaticRestaurantLogger();
+            nonStaticRestaurantLogger2.Joinedrestaurant("Master3 ");
+            Console.WriteLine($"Total number of Chefs joined in my restaurant:{nonStaticRestaurantLogger2.GetChefListCount()}");
 
 
 
